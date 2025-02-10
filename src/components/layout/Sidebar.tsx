de717d84a20ba5f55eda71b27d6847140f650ebf
sindevx <sin.devx@gmail.com>
2025-02-10 15:04:57 +0700

    refactor(lang and env): improve languages and environment variable

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,14 +5,19 @@ import {FaBlog, FaInfoCircle, FaEnvelope, FaUserCircle, FaNewspaper, FaTags, FaT
 import Link from "next/link";
 import { useLanguage } from '@/hooks/useLanguage';
 
+
 const Sidebar: React.FC = () => {
     const { t } = useLanguage();
+
+    //current locale
+    const { currentLocale } = useLanguage();
+
     return (
         <div className="w-64 h-screen bg-gray-800 text-white fixed">
             <h1 className="text-xl font-bold p-4">{t('sidebar.applicationName')}</h1>
             <ul>
 
-                <Link href="/posts">
+                <Link href={`/${currentLocale}/posts`}>
 
                     <li className="p-4 hover:bg-gray-700 cursor-pointer flex items-center">
                         <FaNewspaper className="mr-2"/>
@@ -21,20 +26,20 @@ const Sidebar: React.FC = () => {
                     </li>
                 </Link>
               
-                <Link href='/categories'>
+                <Link href={`/${currentLocale}/categories`}>
                 <li className="p-4 hover:bg-gray-700 cursor-pointer flex items-center">
                     <FaTypo3 className="mr-2"/>
                     {t('sidebar.categories')}
                 </li>
                 </Link>
-                <Link href='/tags'>
+                <Link href={`/${currentLocale}/tags`}>
 
                 <li className="p-4 hover:bg-gray-700 cursor-pointer flex items-center">
                     <FaTags className="mr-2"/>
                     {t('sidebar.tags')}
                 </li>
                 </Link>
-                <Link href='/user'>
+                <Link href={`/${currentLocale}/user`}>
 
                     <li className="p-4 hover:bg-gray-700 cursor-pointer flex items-center">
                         <FaUserCircle className="mr-2"/>
@@ -42,14 +47,14 @@ const Sidebar: React.FC = () => {
                     </li>
                 </Link>
 
-                <Link href="/about">
+                <Link href={`/${currentLocale}/about`}>
 
                 <li className="p-4 hover:bg-gray-700 cursor-pointer flex items-center">
                     <FaInfoCircle className="mr-2"/>
                     {t('sidebar.about')}
                 </li>
                 </Link>
-                <Link href='/contact'>
+                <Link href={`/${currentLocale}/contact`}>
 
                 <li className="p-4 hover:bg-gray-700 cursor-pointer flex items-center">
                     <FaEnvelope className="mr-2"/>
